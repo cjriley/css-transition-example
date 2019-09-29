a9ecb4f8f7bf700d62241ef402e4fec3fb848aab
Clint Riley <rileyclint@gmail.com>
2019-09-29 14:55:05 -0500

    Things

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
+import styled from 'styled-components';
 import './App.css';
 
+const AnimatedDiv = styled.div`
+  height: ${props => props.height};
+  transition: height .2s ease-in-out;
+  border: 2px solid black;
+`
+
 function App() {
+  const [height, setHeight] = useState("80px");
+
+  const toggleHeight = () => {
+    if (height === "80px") {
+      setHeight("160px")
+    } else {
+      setHeight("80px")
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    <AnimatedDiv height={height}>
+      <p>
+        Here is some stuff.
+      </p>
+    </AnimatedDiv>
+    <button onClick={toggleHeight}>Toggle Height</button>
+      </div>
   );
 }
 
